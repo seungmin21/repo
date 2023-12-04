4bc6a9874f0e889ce3994c533eae2ba3ca31c241
ohseungmin <ohmin2886@gmail.com>
2023-12-04 13:00:48 +0900

    css 적용을 위한 미들웨어 설정

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
   res.sendFile(indexPath);
 })
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // 서버 열기가 안되는 사례
 //app.listen(port, () => {
 //  console.log(`서버 http://localhost:${port}가 가동 중입니다.`)
